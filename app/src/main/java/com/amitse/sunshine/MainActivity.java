eba18171f7ef3bbb2d72a2cc0adcf485aa3e049b
Amit Sengupta <sengupta.amit@outlook.com>
2015-11-21 00:08:52 +0530

    Added Toast

--- app/src/main/java/com/amitse/sunshine/MainActivity.java
+++ app/src/main/java/com/amitse/sunshine/MainActivity.java
@@ -1,5 +1,6 @@
 package com.amitse.sunshine;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,8 +22,41 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        Button buttonSpotifyStreamer = (Button) this.findViewById(R.id.buttonSpotifyStreamer);
+        buttonSpotifyStreamer.setOnClickListener(new listen());
+
+        Button buttonBuildItBigger = (Button) this.findViewById(R.id.buttonBuildItBigger);
+        buttonBuildItBigger.setOnClickListener(new listen());
+
+        Button buttonCapstone = (Button) this.findViewById(R.id.buttonCapstone);
+        buttonCapstone.setOnClickListener(new listen());
+        Button buttonLibraryApp = (Button) this.findViewById(R.id.buttonLibraryApp);
+        buttonLibraryApp.setOnClickListener(new listen());
+        Button buttonScoresApp = (Button) this.findViewById(R.id.buttonScoresApp);
+        buttonScoresApp.setOnClickListener(new listen());
+
+        Button buttonXYZReader = (Button) this.findViewById(R.id.buttonXYZReader);
+        buttonXYZReader.setOnClickListener(new listen());
 
     }
+
+    class listen implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            String text  = ((Button)v).getText().toString();
+            showToast("Clicked Button" + text);
+        }
+        public void showToast(String message){
+            Context context = getApplicationContext();
+            CharSequence text = message;
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -41,5 +78,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /** Called when the user touches the button */
+    public void sendMessage(View view) {
+        // Do something in response to button click
     }
 }
